a14bfd02916d3f8b7fa4b482a8d890ee37c37ce6
Diego Gutierrez <gutixtreme@gmail.com>
2023-01-22 18:23:04 +0100

    casi

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 window.onload = function () {
-   
+
    let menu = document.querySelector("#muestra-menu");
    menu.addEventListener("click", muestra);
    let menuizq = document.querySelector(".muestramenu");
    let wrapper = document.querySelector(".wrapper");
    let tapa = document.querySelector(".destapa");
-   let navp=document.querySelector("#navpeque");
+   let navp = document.querySelector("#navpeque");
    let icono = document.querySelectorAll(".fa-solid");
    //funcion para cargar elementos al scrollear
    window.addEventListener('scroll', muestracontenido);
+ 
 
    function muestracontenido() {
+      if (menuizq.style.left != "-100px") {
+         muestra();
+      }
       let elementos = document.getElementsByClassName("elemento");
       let tamañoVentana = window.innerHeight;
 
@@ -18,15 +22,16 @@ window.onload = function () {
 
          elemento.getBoundingClientRect().top < tamañoVentana ? setTimeout(() => {
             elemento.classList.add("visible")
-         }, 150) : elemento.classList.remove("visible");
+         }, 100) : elemento.classList.remove("visible");
       }
 
    };
   
+   
    function muestra() {
       if (menuizq.style.left == "-100px") {
-         icono[0].style.opacity="0";
-         icono[1].style.opacity="1";
+         icono[0].style.opacity = "0";
+         icono[1].style.opacity = "1";
          menuizq.style.left = "0";
          wrapper.style.left = "100px";
          document.body.style.overflow = "hidden";
@@ -35,8 +40,8 @@ window.onload = function () {
          tapa.classList.replace("destapa", "tapa");
 
       } else {
-         icono[0].style.opacity="1";
-         icono[1].style.opacity="0";
+         icono[0].style.opacity = "1";
+         icono[1].style.opacity = "0";
          menuizq.style.left = "-100px";
          wrapper.style.left = "0";
          navp.style.backgroundColor = "rgba(42, 40, 40, 0.816)";
